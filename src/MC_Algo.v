74db71b55da69c795c2149239da12145d13bb719
Peter <plarochkin@gmail.com>
2023-03-06 13:08:44 +0300

    new formulas

--- src/MC_Algo.v
+++ src/MC_Algo.v
@@ -8,10 +8,18 @@ Definition  trans_bool (s1 s2:bool):Prop := ~ (s1 = s2).
 Definition  label_bool(v: nat)(s:bool):Prop := (v = 0 -> s = false) \/ (v = 1 -> s = true).
 Definition  serial_bool: forall w:bool, exists (v:bool), trans_bool w v.
 intros. case w ;[> eexists false|eexists true]; compute; discriminate.
-Defined.  
+Defined.
 
 Definition init_bool(s: bool):Prop := s = false.
 Definition model_bool: sts :=  {| state := bool; trans := trans_bool; init:= init_bool; label := label_bool; serial := serial_bool |}.
+
+(* Theorem ff: forall st: state model_bool, 
+(init model_bool) st -> 
+satisfies (model_bool) (fF) st.
+Proof.
+  intro. intro. unfold satisfies.  discriminate.
+Qed. *)
+
 Theorem fv: forall st: state model_bool, 
 (init model_bool) st -> 
 satisfies (model_bool) (fV 1) st.
